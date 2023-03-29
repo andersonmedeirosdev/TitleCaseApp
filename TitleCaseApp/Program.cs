7bea1b4741d71d2fed3cc3b6b147d1080e8b06a7
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-29 11:45:44 -0300

    Add project files.

---
+++ TitleCaseApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace TitleCaseApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escreva uma frase para manipularmos:");
+            string titulacao = Console.ReadLine();
+
+            string novoTexto = ConverterTitleCase(titulacao);
+            Console.WriteLine(novoTexto);
+            
+            Console.ReadLine();
+        }
+
+        static string ConverterTitleCase(string titulacao)
+        {
+            var textinfo = new CultureInfo("pt-BR", false).TextInfo;
+            return textinfo.ToTitleCase(titulacao);
+        }
+    }
+}
